fe3d000317512ac986ab11d17c416d78948fd02f
Peeke <pkuepers@kaliber.net>
2020-06-15 14:02:03 +0200

    Fix bug where omitting the options object would throw an error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { useIsIntersecting, useWasIntersecting } from '@kaliber/use-is-intersecting'
 
-export function useIsInViewport({ rootMargin, threshold, disabled = undefined }) {
+export function useIsInViewport({ rootMargin = undefined, threshold = undefined, disabled = undefined } = {}) {
   const { 
     ref,
     isIntersecting: isInViewport, 
@@ -9,7 +9,7 @@ export function useIsInViewport({ rootMargin, threshold, disabled = undefined })
   return { isInViewport, wasInViewport, ref }
 }
 
-export function useWasInViewport({ rootMargin, threshold }) {
+export function useWasInViewport({ rootMargin = undefined, threshold = undefined } = {}) {
   const { wasIntersecting: wasInViewport, ref } = useWasIntersecting({ rootMargin, threshold })
   return { wasInViewport, ref }
 }
